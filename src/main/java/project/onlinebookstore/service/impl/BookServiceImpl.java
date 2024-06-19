b69244fbd42a95d35ebb465807b6cd4540b2470c
petro feniak <petro.fenyak@gmail.com>
2024-06-19 12:52:34 +0300

    implemented BookService interface

--- src/main/java/project/onlinebookstore/service/impl/BookServiceImpl.java
+++ src/main/java/project/onlinebookstore/service/impl/BookServiceImpl.java
@@ -1,24 +1,43 @@
 package project.onlinebookstore.service.impl;
 
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import project.onlinebookstore.dto.BookDto;
+import project.onlinebookstore.dto.CreateBookRequestDto;
+import project.onlinebookstore.mapper.BookMapper;
 import project.onlinebookstore.model.Book;
 import project.onlinebookstore.repository.BookRepository;
 import project.onlinebookstore.service.BookService;
 
+@RequiredArgsConstructor
 @Service
 public class BookServiceImpl implements BookService {
-    @Autowired
-    private BookRepository bookRepository;
+    private final BookRepository bookRepository;
+    private final BookMapper bookMapper;
 
     @Override
-    public Book save(Book book) {
-        return bookRepository.save(book);
+    public BookDto save(CreateBookRequestDto bookRequestDto) {
+        Book bookModelForSave =
+                bookRepository.save(bookMapper.toBookModel(bookRequestDto));
+        return bookMapper.toBookDto(bookModelForSave);
     }
 
     @Override
-    public List<Book> findAll() {
-        return bookRepository.findAll();
+    public List<BookDto> findAll() {
+        List<Book> allBookFromRepository = bookRepository.findAll();
+        return allBookFromRepository
+                .stream()
+                .map(bookMapper::toBookDto)
+                .toList();
+    }
+
+    @Override
+    public BookDto findById(Long id) {
+        Book bookById = bookRepository.findById(id).orElseThrow(() ->
+                new EntityNotFoundException("Can not find entity by id:"
+                        + id, new Exception()));
+        return bookMapper.toBookDto(bookById);
     }
 }
