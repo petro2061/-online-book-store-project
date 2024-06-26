999ddd346f48b157d4b0d4d99132f528150155ac
petro feniak <petro.fenyak@gmail.com>
2024-06-26 23:25:19 +0300

    added new methods to BookController

--- src/main/java/project/onlinebookstore/controller/BookController.java
+++ src/main/java/project/onlinebookstore/controller/BookController.java
@@ -2,11 +2,15 @@ package project.onlinebookstore.controller;
 
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import project.onlinebookstore.dto.BookDto;
 import project.onlinebookstore.dto.CreateBookRequestDto;
@@ -28,8 +32,20 @@ public class BookController {
         return bookService.findById(id);
     }
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        bookService.deleteById(id);
+    }
+
     @PostMapping
     public BookDto createBook(@RequestBody CreateBookRequestDto bookDto) {
         return bookService.save(bookDto);
+    }
+
+    @PutMapping("/{id}")
+    private BookDto updateBook(@PathVariable Long id,
+                               @RequestBody CreateBookRequestDto updateBookDto) {
+        return bookService.updateById(id, updateBookDto);
     }
 }
