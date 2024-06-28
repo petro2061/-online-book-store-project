a8967d6a9f036ea5bd3399608a0aba7b6e657445
petro feniak <petro.fenyak@gmail.com>
2024-06-28 23:33:53 +0300

    added TitleSpecificationProvider

---
+++ src/main/java/project/onlinebookstore/repository/book/spec/TitleSpecificationProvider.java
@@ -0,0 +1,21 @@
+package project.onlinebookstore.repository.book.spec;
+
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Component;
+import project.onlinebookstore.model.Book;
+import project.onlinebookstore.repository.SpecificationProvider;
+import java.util.Arrays;
+
+@Component
+public class TitleSpecificationProvider implements SpecificationProvider<Book> {
+    @Override
+    public Specification<Book> getSpecification(String[] params) {
+        return (root, query, criteriaBuilder) -> root.get("title")
+                .in(Arrays.stream(params).toArray());
+    }
+
+    @Override
+    public String getKey() {
+        return "title";
+    }
+}
