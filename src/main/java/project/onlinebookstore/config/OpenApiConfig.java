8b2b5fae65733a9d6b34ff61a6d3d46b46ed8730
petro feniak <petro.fenyak@gmail.com>
2024-07-03 13:14:47 +0300

    fixed problem with codestyle

--- src/main/java/project/onlinebookstore/config/OpenApiConfig.java
+++ src/main/java/project/onlinebookstore/config/OpenApiConfig.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class OpenApiConfig {
-
     @Bean
     public OpenAPI customOpenApi() {
         return new OpenAPI()
