80d6d06743409caff58c00bdc6787ae2c77302a7
petro feniak <petro.fenyak@gmail.com>
2024-07-03 10:11:22 +0300

    added open api configuration

---
+++ src/main/java/project/onlinebookstore/config/OpenApiConfig.java
@@ -0,0 +1,19 @@
+package project.onlinebookstore.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Online Book Store API")
+                        .version("1.0.0")
+                        .description("API for management online book store"));
+    }
+}
