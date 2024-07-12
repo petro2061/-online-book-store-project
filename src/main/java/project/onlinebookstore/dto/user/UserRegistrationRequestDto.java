fe4f1f592eacf7d8caa8bc3609e2007b504fd3a4
petro feniak <petro.fenyak@gmail.com>
2024-07-12 18:47:39 +0300

    added user DTO classes

---
+++ src/main/java/project/onlinebookstore/dto/user/UserRegistrationRequestDto.java
@@ -0,0 +1,27 @@
+package project.onlinebookstore.dto.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+import project.onlinebookstore.validation.FieldMatch;
+import project.onlinebookstore.validation.Password;
+
+@Data
+@FieldMatch(first = "password", second = "repeatPassword")
+public class UserRegistrationRequestDto {
+    @Email
+    @NotBlank(message = "Field title can't be null")
+    private String email;
+    @Length(min = 8, max = 25)
+    @Password
+    private String password;
+    @Length(min = 8, max = 25)
+    private String repeatPassword;
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String lastName;
+    @NotBlank
+    private String shippingAddress;
+}
