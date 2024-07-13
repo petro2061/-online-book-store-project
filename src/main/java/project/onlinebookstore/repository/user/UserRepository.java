40910580e7b279389f35a2059a28553f53a97f03
petro feniak <petro.fenyak@gmail.com>
2024-07-13 13:00:18 +0300

    added new method existsByEmail

--- src/main/java/project/onlinebookstore/repository/user/UserRepository.java
+++ src/main/java/project/onlinebookstore/repository/user/UserRepository.java
@@ -1,11 +1,10 @@
 package project.onlinebookstore.repository.user;
 
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import project.onlinebookstore.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long>,
         JpaSpecificationExecutor<User> {
-    Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
