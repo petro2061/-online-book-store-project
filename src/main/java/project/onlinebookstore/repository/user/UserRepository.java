f51fa2cf9edd6dec7da245374bf39e91ad8b585c
petro feniak <petro.fenyak@gmail.com>
2024-07-18 12:52:04 +0300

    added new method

--- src/main/java/project/onlinebookstore/repository/user/UserRepository.java
+++ src/main/java/project/onlinebookstore/repository/user/UserRepository.java
@@ -1,5 +1,6 @@
 package project.onlinebookstore.repository.user;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import project.onlinebookstore.model.User;
@@ -7,4 +8,6 @@ import project.onlinebookstore.model.User;
 public interface UserRepository extends JpaRepository<User, Long>,
         JpaSpecificationExecutor<User> {
     boolean existsByEmail(String email);
+
+    Optional<User> findByEmail(String email);
 }
