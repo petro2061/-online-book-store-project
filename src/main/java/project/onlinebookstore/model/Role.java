8eb431df82ff584d705e21314773f8f0d411f669
petro feniak <petro.fenyak@gmail.com>
2024-07-18 12:48:01 +0300

    added role model

---
+++ src/main/java/project/onlinebookstore/model/Role.java
@@ -0,0 +1,32 @@
+package project.onlinebookstore.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.springframework.security.core.GrantedAuthority;
+
+@Entity
+@Table(name = "roles")
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private RoleName role;
+
+    @Override
+    public String getAuthority() {
+        return role.name();
+    }
+
+    public enum RoleName {
+        USER,
+        ADMIN
+    }
+}
