12ee419c92413f7988d4e5b13079ef24f5e5e200
petro feniak <petro.fenyak@gmail.com>
2024-07-18 12:53:33 +0300

    modified method register

--- src/main/java/project/onlinebookstore/service/user/impl/UserServiceImpl.java
+++ src/main/java/project/onlinebookstore/service/user/impl/UserServiceImpl.java
@@ -1,11 +1,16 @@
 package project.onlinebookstore.service.user.impl;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import project.onlinebookstore.dto.user.UserRegistrationRequestDto;
 import project.onlinebookstore.dto.user.UserResponseDto;
 import project.onlinebookstore.exception.RegistrationException;
 import project.onlinebookstore.mapper.UserMapper;
+import project.onlinebookstore.model.Role;
+import project.onlinebookstore.model.User;
+import project.onlinebookstore.repository.role.RoleRepository;
 import project.onlinebookstore.repository.user.UserRepository;
 import project.onlinebookstore.service.user.UserService;
 
@@ -14,6 +19,8 @@ import project.onlinebookstore.service.user.UserService;
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final PasswordEncoder passwordEncoder;
+    private final RoleRepository roleRepository;
 
     @Override
     public UserResponseDto register(UserRegistrationRequestDto registrationRequestDto)
@@ -23,7 +30,18 @@ public class UserServiceImpl implements UserService {
                     + registrationRequestDto.getEmail()
                     + " already exist");
         }
-        return userMapper.toUserDto(userRepository
-                .save(userMapper.toUserModel(registrationRequestDto)));
+        Role userRole
+                = roleRepository
+                .findByRole(Role.RoleName.USER)
+                .orElseThrow(() ->
+                        new EntityNotFoundException("Can't find role: USER"));
+
+        User savedUserModel = userMapper.toUserModel(registrationRequestDto);
+
+        savedUserModel
+                .setPassword(passwordEncoder.encode(registrationRequestDto.getPassword()));
+        savedUserModel
+                .getRoles().add(userRole);
+        return userMapper.toUserDto(userRepository.save(savedUserModel));
     }
 }
