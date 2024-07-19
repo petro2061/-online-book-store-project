758dcce12064578c599cd1e3163726a2097cd7c0
petro feniak <petro.fenyak@gmail.com>
2024-07-19 12:12:13 +0300

    added setter and getter annotation and renaming roles

--- src/main/java/project/onlinebookstore/model/Role.java
+++ src/main/java/project/onlinebookstore/model/Role.java
@@ -8,8 +8,12 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 
+@Setter
+@Getter
 @Entity
 @Table(name = "roles")
 public class Role implements GrantedAuthority {
@@ -22,11 +26,11 @@ public class Role implements GrantedAuthority {
 
     @Override
     public String getAuthority() {
-        return "ROLE_" + role.name();
+        return role.name();
     }
 
     public enum RoleName {
-        USER,
-        ADMIN
+        ROLE_USER,
+        ROLE_ADMIN
     }
 }
