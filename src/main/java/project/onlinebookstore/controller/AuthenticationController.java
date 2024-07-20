d19e8300035b1d3bd440627a82616dac10f22d6f
petro feniak <petro.fenyak@gmail.com>
2024-07-21 01:01:52 +0300

    created user login response dto

--- src/main/java/project/onlinebookstore/controller/AuthenticationController.java
+++ src/main/java/project/onlinebookstore/controller/AuthenticationController.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import project.onlinebookstore.dto.user.UserLoginRequestDto;
+import project.onlinebookstore.dto.user.UserLoginResponseDto;
 import project.onlinebookstore.dto.user.UserRegistrationRequestDto;
 import project.onlinebookstore.dto.user.UserResponseDto;
 import project.onlinebookstore.exception.RegistrationException;
+import project.onlinebookstore.security.AuthenticationService;
 import project.onlinebookstore.service.user.UserService;
 
 @RequiredArgsConstructor
@@ -16,11 +19,17 @@ import project.onlinebookstore.service.user.UserService;
 @RequestMapping("/auth")
 public class AuthenticationController {
     private final UserService userService;
+    private final AuthenticationService authenticationService;
 
     @PostMapping("/registration")
     public UserResponseDto register(
             @RequestBody @Valid UserRegistrationRequestDto registrationRequestDto)
             throws RegistrationException {
         return userService.register(registrationRequestDto);
+    }
+
+    @PostMapping("/login")
+    public UserLoginResponseDto login(@RequestBody @Valid UserLoginRequestDto loginRequestDto) {
+        return authenticationService.authenticate(loginRequestDto);
     }
 }
