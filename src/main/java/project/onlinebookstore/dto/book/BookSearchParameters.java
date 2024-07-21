309fe29421b2392063267b5734be6713cb5539df
petro feniak <petro.fenyak@gmail.com>
2024-07-21 23:16:02 +0300

    added new search parameter

--- src/main/java/project/onlinebookstore/dto/book/BookSearchParameters.java
+++ src/main/java/project/onlinebookstore/dto/book/BookSearchParameters.java
@@ -6,5 +6,6 @@ public record BookSearchParameters(
         String[] titles,
         String[] authors,
         String[] isbns,
-        BigDecimal[] prices) {
+        BigDecimal[] prices,
+        Long[] categories) {
 }
