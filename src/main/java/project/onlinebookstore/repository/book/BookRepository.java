63fdb0f7302fe3aa5ac537f6df9bab0966f463a7
petro feniak <petro.fenyak@gmail.com>
2024-07-21 23:15:35 +0300

    added new method

--- src/main/java/project/onlinebookstore/repository/book/BookRepository.java
+++ src/main/java/project/onlinebookstore/repository/book/BookRepository.java
@@ -1,12 +1,16 @@
 package project.onlinebookstore.repository.book;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import project.onlinebookstore.model.Book;
 
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long>,
         JpaSpecificationExecutor<Book> {
+    @Query("SELECT b FROM Book b LEFT JOIN FETCH b.categories c WHERE c.id = :categoryId")
+    List<Book> findAllByCategoryId(Long categoryId);
 
 }
