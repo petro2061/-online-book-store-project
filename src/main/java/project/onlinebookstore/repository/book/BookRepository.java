84010d26a1098ad1beffd78ba8f9dd2dde0a27b4
petro feniak <petro.fenyak@gmail.com>
2024-07-24 18:59:13 +0300

    added Pageable to findAllByCategoryId

--- src/main/java/project/onlinebookstore/repository/book/BookRepository.java
+++ src/main/java/project/onlinebookstore/repository/book/BookRepository.java
@@ -1,6 +1,7 @@
 package project.onlinebookstore.repository.book;
 
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
@@ -11,6 +12,6 @@ import project.onlinebookstore.model.Book;
 public interface BookRepository extends JpaRepository<Book, Long>,
         JpaSpecificationExecutor<Book> {
     @Query("SELECT b FROM Book b LEFT JOIN FETCH b.categories c WHERE c.id = :categoryId")
-    List<Book> findAllByCategoryId(Long categoryId);
+    List<Book> findAllByCategoryId(Long categoryId, Pageable pageable);
 
 }
