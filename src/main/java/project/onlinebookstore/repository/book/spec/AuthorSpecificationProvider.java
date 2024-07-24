f061c531aaeb62d4f0d0b611dd045155e2f37ab8
petro feniak <petro.fenyak@gmail.com>
2024-07-24 18:59:37 +0300

    added fetching for categories field

--- src/main/java/project/onlinebookstore/repository/book/spec/AuthorSpecificationProvider.java
+++ src/main/java/project/onlinebookstore/repository/book/spec/AuthorSpecificationProvider.java
@@ -1,5 +1,6 @@
 package project.onlinebookstore.repository.book.spec;
 
+import jakarta.persistence.criteria.JoinType;
 import java.util.Arrays;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
@@ -9,11 +10,16 @@ import project.onlinebookstore.repository.SpecificationProvider;
 @Component
 public class AuthorSpecificationProvider implements SpecificationProvider<Book> {
     private static final String AUTHOR_FIELD_PARAMETER = "author";
+    private static final String FETCH_FIELD_NAME = "categories";
 
     @Override
     public Specification<Book> getSpecification(String[] params) {
-        return (root, query, criteriaBuilder) ->
-                root.get(AUTHOR_FIELD_PARAMETER).in(Arrays.stream(params).toList());
+        return (root, query, criteriaBuilder) -> {
+            root.fetch(FETCH_FIELD_NAME, JoinType.LEFT);
+            return root
+                    .get(AUTHOR_FIELD_PARAMETER)
+                    .in(Arrays.stream(params).toList());
+        };
     }
 
     @Override
