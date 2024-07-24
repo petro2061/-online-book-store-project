7c0192c28edc42327618d4bcf08f7fc7ac743e8e
petro feniak <petro.fenyak@gmail.com>
2024-07-24 18:57:23 +0300

    added fetching for categories field

--- src/main/java/project/onlinebookstore/repository/book/spec/IsbnSpecificationProvider.java
+++ src/main/java/project/onlinebookstore/repository/book/spec/IsbnSpecificationProvider.java
@@ -1,5 +1,6 @@
 package project.onlinebookstore.repository.book.spec;
 
+import jakarta.persistence.criteria.JoinType;
 import java.util.Arrays;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
@@ -9,11 +10,17 @@ import project.onlinebookstore.repository.SpecificationProvider;
 @Component
 public class IsbnSpecificationProvider implements SpecificationProvider<Book> {
     private static final String ISBN_FIELD_PARAMETER = "isbn";
+    private static final String FETCH_FIELD_NAME = "categories";
 
     @Override
     public Specification<Book> getSpecification(String[] params) {
-        return (root, query, criteriaBuilder) ->
-                root.get(ISBN_FIELD_PARAMETER).in(Arrays.stream(params).toList());
+        return (root, query, criteriaBuilder) -> {
+            root.fetch(FETCH_FIELD_NAME, JoinType.LEFT);
+            return root
+                    .get(ISBN_FIELD_PARAMETER)
+                    .in(Arrays.stream(params).toList());
+        };
+
     }
 
     @Override
