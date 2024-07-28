868308e530e2cf0c59a47bd13d4d09ea6f30ea81
petro feniak <petro.fenyak@gmail.com>
2024-07-28 15:46:47 +0300

    added constructor

--- src/main/java/project/onlinebookstore/model/Book.java
+++ src/main/java/project/onlinebookstore/model/Book.java
@@ -13,12 +13,14 @@ import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.SQLDelete;
 import org.hibernate.annotations.SQLRestriction;
 
 @Setter
 @Getter
+@NoArgsConstructor
 @SQLDelete(sql = "UPDATE books SET is_deleted = true WHERE id =?")
 @SQLRestriction(value = "is_deleted = false")
 @Entity
@@ -45,4 +47,7 @@ public class Book {
     @Column(nullable = false)
     private boolean isDeleted = false;
 
+    public Book(Long id) {
+        this.id = id;
+    }
 }
