f31399cc3f46bf1faf686ebaee093e4554aeb1ba
petro feniak <petro.fenyak@gmail.com>
2024-07-28 15:47:17 +0300

    implemented new method bookFromId

--- src/main/java/project/onlinebookstore/mapper/BookMapper.java
+++ src/main/java/project/onlinebookstore/mapper/BookMapper.java
@@ -1,12 +1,14 @@
 package project.onlinebookstore.mapper;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 import project.onlinebookstore.config.MapperConfig;
 import project.onlinebookstore.dto.book.BookDto;
 import project.onlinebookstore.dto.book.BookDtoWithoutCategoryIds;
@@ -43,5 +45,12 @@ public interface BookMapper {
                 .map(Category::new)
                 .collect(Collectors.toSet());
         book.setCategories(categories);
+    }
+
+    @Named("bookFromId")
+    default Book bookFromId(Long id) {
+        return Optional.ofNullable(id)
+                .map(Book::new)
+                .orElse(null);
     }
 }
