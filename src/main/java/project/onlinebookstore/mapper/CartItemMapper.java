e962f409b9843e56851cbafdac68daafde71638d
petro feniak <petro.fenyak@gmail.com>
2024-07-28 15:50:56 +0300

    added cart item mapper interface

---
+++ src/main/java/project/onlinebookstore/mapper/CartItemMapper.java
@@ -0,0 +1,18 @@
+package project.onlinebookstore.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import project.onlinebookstore.config.MapperConfig;
+import project.onlinebookstore.dto.cartitem.CartItemDto;
+import project.onlinebookstore.dto.cartitem.CreateCartItemRequestDto;
+import project.onlinebookstore.model.CartItem;
+
+@Mapper(config = MapperConfig.class, uses = BookMapper.class)
+public interface CartItemMapper {
+    @Mapping(target = "bookId", source = "book.id")
+    @Mapping(target = "bookTitle", source = "book.title")
+    CartItemDto toDto(CartItem cartItem);
+
+    @Mapping(target = "book", source = "bookId", qualifiedByName = "bookFromId")
+    CartItem toModel(CreateCartItemRequestDto cartItemRequestDto);
+}
