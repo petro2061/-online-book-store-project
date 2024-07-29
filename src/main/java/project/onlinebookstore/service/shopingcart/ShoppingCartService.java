e00496f155eaaebe898a5fa860823bd400e67679
petro feniak <petro.fenyak@gmail.com>
2024-07-29 11:28:09 +0300

    changed configuration of methods

--- src/main/java/project/onlinebookstore/service/shopingcart/ShoppingCartService.java
+++ src/main/java/project/onlinebookstore/service/shopingcart/ShoppingCartService.java
@@ -1,16 +1,21 @@
 package project.onlinebookstore.service.shopingcart;
 
+import project.onlinebookstore.dto.cartitem.CartItemDto;
 import project.onlinebookstore.dto.cartitem.CreateCartItemRequestDto;
+import project.onlinebookstore.dto.cartitem.CreateCartItemUpdateRequestDto;
 import project.onlinebookstore.dto.shoppingcart.ShoppingCartDto;
 
 public interface ShoppingCartService {
     ShoppingCartDto getShoppingCart(Long userId);
 
-    ShoppingCartDto addBookToShoppingCart(
+    CartItemDto addCartItemToShoppingCart(
             Long userId,
             CreateCartItemRequestDto cartItemRequestDto);
 
-    ShoppingCartDto updateCartItem(Long cartItemId, int quantity);
+    CartItemDto updateCartItemInShoppingCart(
+            Long userId,
+            Long cartItemId,
+            CreateCartItemUpdateRequestDto updateRequestDto);
 
-    ShoppingCartDto removeCartItem(Long cartItemId);
+    CartItemDto removeCartItemFromShoppingCart(Long userId, Long cartItemId);
 }
