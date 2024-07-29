1cebc15550e57b7271cfa5f691ef18ae986b8263
petro feniak <petro.fenyak@gmail.com>
2024-07-29 11:25:40 +0300

    added new custom method

--- src/main/java/project/onlinebookstore/repository/cartitem/CartItemRepository.java
+++ src/main/java/project/onlinebookstore/repository/cartitem/CartItemRepository.java
@@ -1,9 +1,16 @@
 package project.onlinebookstore.repository.cartitem;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import project.onlinebookstore.model.CartItem;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
+
+    @Query("SELECT ci FROM CartItem ci "
+            + "LEFT JOIN FETCH ci.book "
+            + "WHERE ci.shoppingCart.id = :shoppingCartId AND ci.book.id = :bookId")
+    Optional<CartItem> findByShoppingCartIdAndBookId(Long shoppingCartId, Long bookId);
 }
