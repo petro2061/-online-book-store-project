6b42fddbb9ad7bfdfa2d8222e364d457adb253f5
petro feniak <petro.fenyak@gmail.com>
2024-08-02 11:43:27 +0300

    changed custom query

--- src/main/java/project/onlinebookstore/repository/shoppingcart/ShoppingCartRepository.java
+++ src/main/java/project/onlinebookstore/repository/shoppingcart/ShoppingCartRepository.java
@@ -5,12 +5,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import project.onlinebookstore.model.ShoppingCart;
-import project.onlinebookstore.model.User;
 
 @Repository
-public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, User> {
+public interface ShoppingCartRepository extends JpaRepository<ShoppingCart, Long> {
     @Query("SELECT sc FROM ShoppingCart sc "
             + "LEFT JOIN FETCH sc.cartItems "
+            + "LEFT JOIN FETCH sc.cartItems.book "
             + "WHERE sc.user.id = :userId")
     Optional<ShoppingCart> findByUserId(Long userId);
 }
