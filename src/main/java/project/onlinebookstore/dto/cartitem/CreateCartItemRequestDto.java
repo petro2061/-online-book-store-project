5feedc4ac598a48a1ed22362a3f1a151d6bd06ef
petro feniak <petro.fenyak@gmail.com>
2024-08-05 00:05:56 +0300

    changed message in annotation

--- src/main/java/project/onlinebookstore/dto/cartitem/CreateCartItemRequestDto.java
+++ src/main/java/project/onlinebookstore/dto/cartitem/CreateCartItemRequestDto.java
@@ -3,7 +3,7 @@ package project.onlinebookstore.dto.cartitem;
 import jakarta.validation.constraints.Positive;
 
 public record CreateCartItemRequestDto(
-        @Positive(message = "Field bookId can't be null")
+        @Positive(message = "Field bookId can't be less one")
         Long bookId,
         @Positive(message = "Quantity can't be less one")
         int quantity
