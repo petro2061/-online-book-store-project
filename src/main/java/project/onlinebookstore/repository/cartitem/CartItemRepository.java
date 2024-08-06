4b5755ba357f1802da781be0abccdd9d72ffd809
petro feniak <petro.fenyak@gmail.com>
2024-08-06 18:43:36 +0300

    deleted annotation

--- src/main/java/project/onlinebookstore/repository/cartitem/CartItemRepository.java
+++ src/main/java/project/onlinebookstore/repository/cartitem/CartItemRepository.java
@@ -3,10 +3,8 @@ package project.onlinebookstore.repository.cartitem;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import project.onlinebookstore.model.CartItem;
 
-@Repository
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
     @Query("SELECT ci FROM CartItem ci "
