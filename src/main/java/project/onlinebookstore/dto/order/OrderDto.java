a39a340fa9f1bc8985832c425308c4bc07dfa8ad
petro feniak <petro.fenyak@gmail.com>
2024-08-07 23:58:32 +0300

    changed List type

--- src/main/java/project/onlinebookstore/dto/order/OrderDto.java
+++ src/main/java/project/onlinebookstore/dto/order/OrderDto.java
@@ -4,14 +4,14 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Data;
+import project.onlinebookstore.dto.orderitem.OrderItemDto;
 import project.onlinebookstore.model.Order;
-import project.onlinebookstore.model.OrderItem;
 
 @Data
 public class OrderDto {
     private Long id;
     private Long userId;
-    private List<OrderItem> orderItems;
+    private List<OrderItemDto> orderItems;
     private LocalDateTime orderDate;
     private BigDecimal total;
     private Order.Status status;
