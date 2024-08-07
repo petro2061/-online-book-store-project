5ccf919a8874b380b53daade54c24c7ffd731825
petro feniak <petro.fenyak@gmail.com>
2024-08-07 23:59:18 +0300

    added OrderMapper interface

---
+++ src/main/java/project/onlinebookstore/mapper/OrderMapper.java
@@ -0,0 +1,55 @@
+package project.onlinebookstore.mapper;
+
+import java.math.BigDecimal;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+import project.onlinebookstore.config.MapperConfig;
+import project.onlinebookstore.dto.order.CreateOrderRequestDto;
+import project.onlinebookstore.dto.order.OrderDto;
+import project.onlinebookstore.model.CartItem;
+import project.onlinebookstore.model.Order;
+import project.onlinebookstore.model.OrderItem;
+import project.onlinebookstore.model.ShoppingCart;
+
+@Mapper(config = MapperConfig.class, uses = OrderItemMapper.class)
+public interface OrderMapper {
+
+    @Mapping(target = "userId", source = "user.id")
+    @Mapping(target = "orderItems", source = "orderItems")
+    @Mapping(target = "orderDate", dateFormat = "yyyy-MM-dd HH:mm")
+    OrderDto toDto(Order order);
+
+    @Mapping(target = "user", source = "cart.user")
+    @Mapping(target = "total", source = "cart.cartItems", qualifiedByName = "getTotalPrice")
+    @Mapping(target = "orderItems", source = "cart.cartItems", qualifiedByName = "toOrderItems")
+    @Mapping(target = "status", ignore = true)
+    @Mapping(target = "orderDate", ignore = true)
+    Order toModel(ShoppingCart cart, CreateOrderRequestDto orderRequestDto);
+
+    @Named("getTotalPrice")
+    default BigDecimal getTotalPrice(Set<CartItem> cartItems) {
+        return cartItems
+                .stream()
+                .map(orderItem ->
+                        orderItem.getBook().getPrice()
+                                .multiply(BigDecimal.valueOf(orderItem.getQuantity())))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    @Named("toOrderItems")
+    default Set<OrderItem> toOrderItems(Set<CartItem> cartItems) {
+        return cartItems
+                .stream()
+                .map(cartItem -> {
+                    OrderItem orderItem = new OrderItem();
+                    orderItem.setBook(cartItem.getBook());
+                    orderItem.setQuantity(cartItem.getQuantity());
+                    orderItem.setPrice(cartItem.getBook().getPrice());
+                    return orderItem;
+                })
+                .collect(Collectors.toSet());
+    }
+}
