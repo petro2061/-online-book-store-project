485bb363799394ca3dc9e46be08b39af0d7d48ca
petro feniak <petro.fenyak@gmail.com>
2024-08-12 11:11:15 +0300

    added new properties

--- src/main/java/project/onlinebookstore/mapper/OrderMapper.java
+++ src/main/java/project/onlinebookstore/mapper/OrderMapper.java
@@ -5,9 +5,11 @@ import java.util.Set;
 import java.util.stream.Collectors;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.Named;
 import project.onlinebookstore.config.MapperConfig;
 import project.onlinebookstore.dto.order.CreateOrderRequestDto;
+import project.onlinebookstore.dto.order.CreateOrderUpdateRequestDto;
 import project.onlinebookstore.dto.order.OrderDto;
 import project.onlinebookstore.model.CartItem;
 import project.onlinebookstore.model.Order;
@@ -22,12 +24,24 @@ public interface OrderMapper {
     @Mapping(target = "orderDate", dateFormat = "yyyy-MM-dd HH:mm")
     OrderDto toDto(Order order);
 
+    @Mapping(target = "id", ignore = true)
     @Mapping(target = "user", source = "cart.user")
     @Mapping(target = "total", source = "cart.cartItems", qualifiedByName = "getTotalPrice")
     @Mapping(target = "orderItems", source = "cart.cartItems", qualifiedByName = "toOrderItems")
+    @Mapping(target = "shippingAddress", source = "orderRequestDto.shippingAddress")
     @Mapping(target = "status", ignore = true)
     @Mapping(target = "orderDate", ignore = true)
     Order toModel(ShoppingCart cart, CreateOrderRequestDto orderRequestDto);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "user", ignore = true)
+    @Mapping(target = "total", ignore = true)
+    @Mapping(target = "orderDate", ignore = true)
+    @Mapping(target = "shippingAddress", ignore = true)
+    @Mapping(target = "orderItems", ignore = true)
+    @Mapping(target = "deleted", ignore = true)
+    void updateOrderStatus(CreateOrderUpdateRequestDto updateRequestDto,
+                           @MappingTarget Order order);
 
     @Named("getTotalPrice")
     default BigDecimal getTotalPrice(Set<CartItem> cartItems) {
