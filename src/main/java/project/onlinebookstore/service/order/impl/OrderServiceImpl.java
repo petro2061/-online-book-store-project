22904c81e0eb89d16ef9e942fea590aeb291f3c8
petro feniak <petro.fenyak@gmail.com>
2024-08-12 11:12:30 +0300

    realised new methods from interface

--- src/main/java/project/onlinebookstore/service/order/impl/OrderServiceImpl.java
+++ src/main/java/project/onlinebookstore/service/order/impl/OrderServiceImpl.java
@@ -1,16 +1,23 @@
 package project.onlinebookstore.service.order.impl;
 
 import jakarta.transaction.Transactional;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import project.onlinebookstore.dto.order.CreateOrderRequestDto;
+import project.onlinebookstore.dto.order.CreateOrderUpdateRequestDto;
 import project.onlinebookstore.dto.order.OrderDto;
+import project.onlinebookstore.dto.orderitem.OrderItemDto;
 import project.onlinebookstore.exception.EntityNotFoundException;
 import project.onlinebookstore.exception.OrderProcessingException;
+import project.onlinebookstore.mapper.OrderItemMapper;
 import project.onlinebookstore.mapper.OrderMapper;
 import project.onlinebookstore.model.Order;
+import project.onlinebookstore.model.OrderItem;
 import project.onlinebookstore.model.ShoppingCart;
 import project.onlinebookstore.repository.order.OrderRepository;
+import project.onlinebookstore.repository.orderitem.OrderItemRepository;
 import project.onlinebookstore.repository.shoppingcart.ShoppingCartRepository;
 import project.onlinebookstore.service.order.OrderService;
 
@@ -19,7 +26,9 @@ import project.onlinebookstore.service.order.OrderService;
 public class OrderServiceImpl implements OrderService {
     private final ShoppingCartRepository shoppingCartRepository;
     private final OrderMapper orderMapper;
+    private final OrderItemMapper orderItemMapper;
     private final OrderRepository orderRepository;
+    private final OrderItemRepository orderItemRepository;
 
     @Override
     @Transactional
@@ -31,14 +40,63 @@ public class OrderServiceImpl implements OrderService {
         if (shoppingCart.getCartItems().isEmpty()) {
             throw new OrderProcessingException("Cart items is empty for user: " + userId);
         }
-        Order order = orderMapper.toModel(shoppingCart, orderRequestDto);
 
+        Order order = orderMapper.toModel(shoppingCart, orderRequestDto);
         order.getOrderItems().forEach(orderItem -> orderItem.setOrder(order));
+
         Order savedOrder = orderRepository.save(order);
 
         shoppingCart.getCartItems().clear();
         shoppingCartRepository.save(shoppingCart);
 
         return orderMapper.toDto(savedOrder);
+    }
+
+    @Override
+    public List<OrderDto> getAllOrder(Long userId, Pageable pageable) {
+        List<Order> byUserIdWithItems
+                = orderRepository.findByUserIdWithItems(userId, pageable);
+        return byUserIdWithItems.stream()
+                .map(orderMapper::toDto)
+                .toList();
+    }
+
+    @Override
+    @Transactional
+    public OrderDto updateOrderStatus(
+            Long orderId,
+            CreateOrderUpdateRequestDto updateRequestDto
+    ) {
+        Order order = orderRepository.findByUserIdAndOrderId(orderId)
+                .orElseThrow(() ->
+                        new EntityNotFoundException("Can't find order by orderId: "
+                                + orderId));
+        orderMapper.updateOrderStatus(updateRequestDto, order);
+        return orderMapper.toDto(orderRepository.save(order));
+    }
+
+    @Override
+    public List<OrderItemDto> getAllOrderItemsByOrderId(Long userId, Long orderId) {
+        return orderItemRepository.getAllOrderItemsByOrderId(userId, orderId).stream()
+                .map(orderItemMapper::toDto)
+                .toList();
+    }
+
+    @Override
+    public OrderItemDto getOrderItemByOrderIdAndOrderItemId(
+            Long userId,
+            Long orderId,
+            Long orderItemId) {
+        OrderItem orderItem =
+                orderItemRepository
+                        .getOrderItemByOrderIdAndOrderItemId(userId, orderId, orderItemId)
+                        .orElseThrow(() ->
+                                new EntityNotFoundException("Can't find order item with Id: "
+                                        + orderItemId
+                                        + " by userId: "
+                                        + userId
+                                        + " and orderId: "
+                                        + orderId));
+        return orderItemMapper.toDto(orderItem);
     }
 }
