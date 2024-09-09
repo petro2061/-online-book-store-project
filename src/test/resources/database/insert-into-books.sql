b598ffdd3ff71f5e3ed50dd5893263d96b69c009
petro feniak <petro.fenyak@gmail.com>
2024-09-09 23:20:11 +0300

    fixed values

--- src/test/resources/database/insert-into-books.sql
+++ src/test/resources/database/insert-into-books.sql
@@ -1,4 +1,4 @@
 INSERT INTO books (id, title, author, isbn, price, description, cover_image)
-VALUES(1, 'Robots and Empire', 'Isaac Asimov', '9780008277796', 360.00, 'This is a sample book description. 1', 'http://example.com/cover1.jpg');
+VALUES(1, 'Robots and Empire', 'Isaac Asimov', '9780008277796', 360.000, 'This is a sample book description. 1', 'http://example.com/cover1.jpg');
 INSERT INTO books (id, title, author, isbn, price, description, cover_image)
-VALUES(2, 'Murder on the Orient Express', 'Agatha Christie', '978-0063160354', 13.99, 'This is a sample book description. 2', 'http://example.com/cover2.jpg');
+VALUES(2, 'Murder on the Orient Express', 'Agatha Christie', '978-0063160354', 13.990, 'This is a sample book description. 2', 'http://example.com/cover2.jpg');
